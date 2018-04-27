64763c562e0ad1b9c2eb73c1cd312dd419e77dff
PurpleSpy <36999286+PurpleSpy@users.noreply.github.com>
2018-04-26 19:14:48 -0700

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,303 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.TrackBar9 = New System.Windows.Forms.TrackBar()
+        Me.TrackBar8 = New System.Windows.Forms.TrackBar()
+        Me.TrackBar7 = New System.Windows.Forms.TrackBar()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.TrackBar6 = New System.Windows.Forms.TrackBar()
+        Me.TrackBar5 = New System.Windows.Forms.TrackBar()
+        Me.TrackBar4 = New System.Windows.Forms.TrackBar()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TrackBar3 = New System.Windows.Forms.TrackBar()
+        Me.TrackBar2 = New System.Windows.Forms.TrackBar()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        CType(Me.TrackBar9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar7, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.TrackBar6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.TrackBar3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.Controls.Add(Me.GroupBox3)
+        Me.Panel1.Controls.Add(Me.GroupBox2)
+        Me.Panel1.Controls.Add(Me.GroupBox1)
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Location = New System.Drawing.Point(11, 375)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1009, 199)
+        Me.Panel1.TabIndex = 0
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox3.Controls.Add(Me.TrackBar9)
+        Me.GroupBox3.Controls.Add(Me.TrackBar8)
+        Me.GroupBox3.Controls.Add(Me.TrackBar7)
+        Me.GroupBox3.Location = New System.Drawing.Point(785, 3)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(200, 144)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "blue"
+        '
+        'TrackBar9
+        '
+        Me.TrackBar9.Location = New System.Drawing.Point(119, 19)
+        Me.TrackBar9.Maximum = 510
+        Me.TrackBar9.Minimum = -510
+        Me.TrackBar9.Name = "TrackBar9"
+        Me.TrackBar9.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.TrackBar9.Size = New System.Drawing.Size(45, 104)
+        Me.TrackBar9.TabIndex = 0
+        Me.TrackBar9.Tag = "22"
+        Me.TrackBar9.TickStyle = System.Windows.Forms.TickStyle.Both
+        '
+        'TrackBar8
+        '
+        Me.TrackBar8.Location = New System.Drawing.Point(68, 19)
+        Me.TrackBar8.Maximum = 510
+        Me.TrackBar8.Minimum = -510
+        Me.TrackBar8.Name = "TrackBar8"
+        Me.TrackBar8.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.TrackBar8.Size = New System.Drawing.Size(45, 104)
+        Me.TrackBar8.TabIndex = 0
+        Me.TrackBar8.Tag = "21"
+        Me.TrackBar8.TickStyle = System.Windows.Forms.TickStyle.Both
+        '
+        'TrackBar7
+        '
+        Me.TrackBar7.Location = New System.Drawing.Point(17, 19)
+        Me.TrackBar7.Maximum = 510
+        Me.TrackBar7.Minimum = -510
+        Me.TrackBar7.Name = "TrackBar7"
+        Me.TrackBar7.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.TrackBar7.Size = New System.Drawing.Size(45, 104)
+        Me.TrackBar7.TabIndex = 0
+        Me.TrackBar7.Tag = "20"
+        Me.TrackBar7.TickStyle = System.Windows.Forms.TickStyle.Both
+        Me.TrackBar7.Value = 255
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox2.Controls.Add(Me.TrackBar6)
+        Me.GroupBox2.Controls.Add(Me.TrackBar5)
+        Me.GroupBox2.Controls.Add(Me.TrackBar4)
+        Me.GroupBox2.Location = New System.Drawing.Point(579, 3)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(200, 144)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "green"
+        '
+        'TrackBar6
+        '
+        Me.TrackBar6.Location = New System.Drawing.Point(119, 19)
+        Me.TrackBar6.Maximum = 510
+        Me.TrackBar6.Minimum = -510
+        Me.TrackBar6.Name = "TrackBar6"
+        Me.TrackBar6.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.TrackBar6.Size = New System.Drawing.Size(45, 104)
+        Me.TrackBar6.TabIndex = 0
+        Me.TrackBar6.Tag = "12"
+        Me.TrackBar6.TickStyle = System.Windows.Forms.TickStyle.Both
+        '
+        'TrackBar5
+        '
+        Me.TrackBar5.Location = New System.Drawing.Point(68, 19)
+        Me.TrackBar5.Maximum = 510
+        Me.TrackBar5.Minimum = -510
+        Me.TrackBar5.Name = "TrackBar5"
+        Me.TrackBar5.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.TrackBar5.Size = New System.Drawing.Size(45, 104)
+        Me.TrackBar5.TabIndex = 0
+        Me.TrackBar5.Tag = "11"
+        Me.TrackBar5.TickStyle = System.Windows.Forms.TickStyle.Both
+        '
+        'TrackBar4
+        '
+        Me.TrackBar4.Location = New System.Drawing.Point(17, 19)
+        Me.TrackBar4.Maximum = 510
+        Me.TrackBar4.Minimum = -510
+        Me.TrackBar4.Name = "TrackBar4"
+        Me.TrackBar4.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.TrackBar4.Size = New System.Drawing.Size(45, 104)
+        Me.TrackBar4.TabIndex = 0
+        Me.TrackBar4.Tag = "10"
+        Me.TrackBar4.TickStyle = System.Windows.Forms.TickStyle.Both
+        Me.TrackBar4.Value = 255
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox1.Controls.Add(Me.TrackBar3)
+        Me.GroupBox1.Controls.Add(Me.TrackBar2)
+        Me.GroupBox1.Controls.Add(Me.TrackBar1)
+        Me.GroupBox1.Location = New System.Drawing.Point(373, 3)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 144)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Red"
+        '
+        'TrackBar3
+        '
+        Me.TrackBar3.Location = New System.Drawing.Point(119, 19)
+        Me.TrackBar3.Maximum = 510
+        Me.TrackBar3.Minimum = -510
+        Me.TrackBar3.Name = "TrackBar3"
+        Me.TrackBar3.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.TrackBar3.Size = New System.Drawing.Size(45, 104)
+        Me.TrackBar3.TabIndex = 0
+        Me.TrackBar3.Tag = "02"
+        Me.TrackBar3.TickStyle = System.Windows.Forms.TickStyle.Both
+        '
+        'TrackBar2
+        '
+        Me.TrackBar2.Location = New System.Drawing.Point(68, 19)
+        Me.TrackBar2.Maximum = 510
+        Me.TrackBar2.Minimum = -510
+        Me.TrackBar2.Name = "TrackBar2"
+        Me.TrackBar2.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.TrackBar2.Size = New System.Drawing.Size(45, 104)
+        Me.TrackBar2.TabIndex = 0
+        Me.TrackBar2.Tag = "01"
+        Me.TrackBar2.TickStyle = System.Windows.Forms.TickStyle.Both
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Location = New System.Drawing.Point(17, 19)
+        Me.TrackBar1.Maximum = 510
+        Me.TrackBar1.Minimum = -510
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.TrackBar1.Size = New System.Drawing.Size(45, 104)
+        Me.TrackBar1.TabIndex = 0
+        Me.TrackBar1.Tag = "00"
+        Me.TrackBar1.TickStyle = System.Windows.Forms.TickStyle.Both
+        Me.TrackBar1.Value = 255
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Location = New System.Drawing.Point(842, 170)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Reset image"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(930, 170)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Reset Matrix"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PictureBox1.Location = New System.Drawing.Point(11, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(1005, 357)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScroll = True
+        Me.ClientSize = New System.Drawing.Size(1028, 580)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Panel1)
+        Me.DoubleBuffered = True
+        Me.KeyPreview = True
+        Me.Name = "Form1"
+        Me.Text = "Color Matrix Test"
+        Me.Panel1.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        CType(Me.TrackBar9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar7, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.TrackBar6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.TrackBar3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents TrackBar9 As TrackBar
+    Friend WithEvents TrackBar8 As TrackBar
+    Friend WithEvents TrackBar7 As TrackBar
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents TrackBar6 As TrackBar
+    Friend WithEvents TrackBar5 As TrackBar
+    Friend WithEvents TrackBar4 As TrackBar
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TrackBar3 As TrackBar
+    Friend WithEvents TrackBar2 As TrackBar
+    Friend WithEvents TrackBar1 As TrackBar
+End Class
---
+++ Form1.vb
@@ -0,0 +1,277 @@
+﻿Public Class Form1
+    Dim lastimg As Image = Nothing
+    Dim crx As New Drawing.Imaging.ColorMatrix()
+    Dim loaded As Boolean = False
+    Dim lastadustment As Image = Nothing
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PictureBox1.SendToBack()
+        Dim channels As String() = {"Red", "Green", "Blue", "Alpha", "Multiplier vs column"}
+
+
+
+        For ypos As Integer = 0 To 4
+            For xpos As Integer = 0 To 4
+                Dim vg As New NumericUpDown
+                vg.Name = "matrix" & ypos & xpos
+
+                vg.Anchor = AnchorStyles.Top Or AnchorStyles.Left
+                vg.DecimalPlaces = 4
+                vg.Increment = 0.01
+                vg.Minimum = -2
+                vg.Maximum = 2
+                vg.Top = vg.PreferredSize.Height * ypos
+                vg.Left = vg.PreferredSize.Width * xpos
+                vg.Width = vg.PreferredSize.Width
+                vg.Height = vg.PreferredSize.Height
+                vg.Value = crx.Item(xpos, ypos)
+                Panel1.Controls.Add(vg)
+                removehandlers(vg)
+                AddHandler vg.ValueChanged, AddressOf numupdownch
+                AddHandler vg.Enter, AddressOf getnameofnum
+            Next
+            Dim srip As New Label
+            srip.Text = channels(ypos)
+            srip.Left = (Panel1.Controls(Panel1.Controls.Count - 1).PreferredSize.Width * 5) + 5
+            srip.Top = Panel1.Controls(Panel1.Controls.Count - 1).PreferredSize.Height * ypos + 5
+            Panel1.Controls.Add(srip)
+        Next
+        If My.Application.CommandLineArgs.Count = 1 Then
+            If IO.File.Exists(My.Application.CommandLineArgs(0)) Then
+                applyImage(My.Application.CommandLineArgs(0))
+            Else
+                Try
+                    Dim gx As New Uri(My.Application.CommandLineArgs(0))
+                    Using svp As New Net.WebClient
+                        Dim dmem As New IO.MemoryStream(svp.DownloadData(gx.OriginalString))
+                        Dim sgp As Image = Image.FromStream(dmem)
+                        applyImage(sgp)
+                    End Using
+
+                Catch ex As Exception
+
+                End Try
+            End If
+        Else
+            Using stx As New OpenFileDialog
+                stx.Filter = "Image file *.png,*.jpg|*.png;*.jpg"
+                stx.FilterIndex = 1
+                stx.Title = "Open an image for processing"
+                If stx.ShowDialog = DialogResult.OK Then
+                    Try
+                        applyImage(stx.FileName)
+                    Catch ex As Exception
+                        MessageBox.Show(stx.FileName & " could not be opened")
+                        Me.Close()
+                        Exit Sub
+                    End Try
+
+                Else
+                    Me.Close()
+                    Exit Sub
+                End If
+            End Using
+        End If
+        addhandlers()
+        loaded = True
+    End Sub
+    Sub getnameofnum(sender As Object, e As EventArgs)
+        Me.Text = sender.name
+    End Sub
+
+    Sub numupdownch(sender As Object, e As EventArgs)
+        If Not loaded Or lastimg Is Nothing Then
+            Exit Sub
+        End If
+        Dim drawmatrix As New Drawing.Imaging.ColorMatrix
+        removehandlers(sender)
+        For ypos As Integer = 0 To 4
+            For xpos As Integer = 0 To 4
+                Dim rx As NumericUpDown = Panel1.Controls("matrix" & ypos & xpos)
+                drawmatrix(xpos, ypos) = rx.Value
+            Next
+        Next
+        Dim bckimg As Image = New Bitmap(lastimg.Width, lastimg.Height)
+        Using sce As Drawing.Graphics = Drawing.Graphics.FromImage(bckimg)
+            Dim tx As New Imaging.ImageAttributes
+            tx.SetColorMatrix(drawmatrix)
+
+            sce.DrawImage(lastimg, New Rectangle(New Point(0, 0), bckimg.Size), 0, 0, bckimg.Width, bckimg.Height, GraphicsUnit.Pixel, tx)
+            lastadustment = bckimg.Clone
+            PictureBox1.Image = bckimg.Clone
+            sce.Dispose()
+            bckimg.Dispose()
+        End Using
+        addhandlers()
+    End Sub
+
+    Private Sub Form1_DragEnter(sender As Object, e As DragEventArgs) Handles Me.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            Dim cst As String() = e.Data.GetData(DataFormats.FileDrop)
+
+            If cst.Length = 1 Then
+                Try
+                    Dim cimg As Drawing.Image = Drawing.Image.FromFile(cst(0))
+                    cimg.Dispose()
+                    e.Effect = DragDropEffects.Copy
+                    Exit Sub
+                Catch ex As Exception
+
+                End Try
+
+            End If
+
+        End If
+        If e.Data.GetDataPresent(DataFormats.Bitmap) Then
+            Dim cst As Image = e.Data.GetData(DataFormats.Bitmap)
+
+            If cst IsNot Nothing Then
+                Try
+                    Dim cimg As Drawing.Image = cst
+                    cimg.Dispose()
+                    e.Effect = DragDropEffects.Copy
+                    Exit Sub
+                Catch ex As Exception
+
+                End Try
+
+            End If
+
+        End If
+        e.Effect = DragDropEffects.None
+    End Sub
+
+    Private Sub Form1_DragDrop(sender As Object, e As DragEventArgs) Handles Me.DragDrop
+        If e.Effect = DragDropEffects.Copy Then
+            Try
+
+                applyImage(e.Data.GetData(DataFormats.FileDrop)(0))
+
+
+            Catch ex As Exception
+
+            End Try
+        End If
+    End Sub
+
+
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Keys.V And e.Modifiers = Keys.Control Then
+            If Clipboard.ContainsImage Then
+                applyImage(Clipboard.GetImage)
+            End If
+        End If
+    End Sub
+
+    Public Sub applyImage(path As String)
+
+        Dim csa As Image = Image.FromFile(path)
+        applyImage(csa)
+
+
+    End Sub
+
+    Public Sub applyImage(pic As Image)
+        lastimg = pic.Clone
+        PictureBox1.Image = lastimg.Clone
+    End Sub
+
+    Public Sub resetMatrix()
+        loaded = False
+        Dim drawmatrix As New Drawing.Imaging.ColorMatrix
+
+        For ypos As Integer = 0 To 4
+            For xpos As Integer = 0 To 4
+                Dim rx As NumericUpDown = Panel1.Controls("matrix" & ypos & xpos)
+                rx.Value = drawmatrix(xpos, ypos)
+                removehandlers(rx)
+            Next
+
+
+        Next
+        loaded = True
+        numupdownch(Panel1.Controls("matrix00"), Nothing)
+        addhandlers()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        resetMatrix()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If lastimg IsNot Nothing Then
+            applyImage(lastimg)
+        End If
+    End Sub
+
+
+
+
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs)
+        Dim send As TrackBar = sender
+        Dim ctl As NumericUpDown = Me.Controls.Find("Matrix" & send.Tag, True)(0)
+        ctl.Value = send.Value / 255
+
+    End Sub
+
+    Private Sub removehandlers(modifed As NumericUpDown)
+        For i As Integer = 1 To 9
+
+            Dim ctrack As TrackBar = Me.Controls.Find("Trackbar" & i, True)(0)
+            RemoveHandler ctrack.Scroll, AddressOf TrackBar1_Scroll
+
+            If modifed.Name.IndexOf(ctrack.Tag) > -1 Then
+                ctrack.Value = modifed.Value * 255
+            End If
+
+        Next
+    End Sub
+    Private Sub addhandlers()
+        For i As Integer = 1 To 9
+
+            Dim ctrack As TrackBar = Me.Controls.Find("Trackbar" & i, True)(0)
+            AddHandler ctrack.Scroll, AddressOf TrackBar1_Scroll
+        Next
+    End Sub
+
+    Private Sub PictureBox1_MouseEnter(sender As Object, e As EventArgs) Handles PictureBox1.MouseEnter
+        If lastimg IsNot Nothing Then
+            sender.cursor = Cursors.Hand
+        Else
+            sender.cursor = Cursors.No
+        End If
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        If lastadustment IsNot Nothing Then
+
+
+            Using srp As New SaveFileDialog
+                srp.Filter = "Png *.png|*.png"
+                srp.FilterIndex = 1
+                If srp.ShowDialog = DialogResult.OK Then
+                    Dim myEncoderParameters As New Drawing.Imaging.EncoderParameters(1)
+                    Dim myEncoder As System.Drawing.Imaging.Encoder = System.Drawing.Imaging.Encoder.Quality
+                    Dim myEncoderParameter As New System.Drawing.Imaging.EncoderParameter(myEncoder, 100&)
+                    myEncoderParameters.Param(0) = myEncoderParameter
+                    Dim pngencoder As Drawing.Imaging.ImageCodecInfo = GetEncoder(Drawing.Imaging.ImageFormat.Png)
+                    lastadustment.Save(srp.FileName, pngencoder, myEncoderParameters)
+                End If
+
+            End Using
+        End If
+    End Sub
+    Private Function GetEncoder(ByVal format As Drawing.Imaging.ImageFormat) As Drawing.Imaging.ImageCodecInfo
+
+        Dim codecs As Drawing.Imaging.ImageCodecInfo() = Drawing.Imaging.ImageCodecInfo.GetImageDecoders()
+
+        Dim codec As Drawing.Imaging.ImageCodecInfo
+        For Each codec In codecs
+            If codec.FormatID = format.Guid Then
+                Return codec
+            End If
+        Next codec
+        Return Nothing
+
+    End Function
+End Class
